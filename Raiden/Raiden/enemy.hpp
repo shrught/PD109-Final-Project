c688d7921a9415954104c506b63d761b1def0eb9
audreyhsiao <51323445+audreyhsiao@users.noreply.github.com>
2021-01-09 16:55:28 +0800

    enemy

---
+++ Raiden/Raiden/enemy.hpp
@@ -0,0 +1,47 @@
+#include <ctime>
+#include <iostream>
+
+#include "config.h"
+#include "game.hpp"
+using namespace sf;
+
+class Enemy {
+ public:
+  Enemy(float x_, float y_, float speed, RenderWindow&);
+  Sprite pdogs;
+
+ private:
+  Texture graphic;
+  void update();
+
+  // float distance;
+  float speed;
+
+  // void update(float dt);
+
+  void render(sf::RenderTarget& target);
+};
+
+Enemy::Enemy(float x_, float y_, float speed, RenderWindow& window) {
+  graphic.loadFromFile("Raiden/Resources/images/pdogs.png");
+
+  pdogs.setPosition(Vector2f(x_, y_));
+  window.draw(pdogs);
+
+  for (int i = 0; i < 100; i++) {
+    update();
+  }
+}
+
+void Enemy::update() {
+  Clock clock;
+  double dt;
+  dt = clock.restart().asSeconds();
+  pdogs.move(0, speed * dt);
+}
+
+// const int MAX_ENTITES = 100;
+
+// std::vector<Enemy *> v;
+
+// Pdogs* pdogs = new Pdogs("pdogs.png");
--- Raiden/Raiden/game.cpp
+++ Raiden/Raiden/game.cpp
@@ -8,96 +8,83 @@
 
 #include "game.hpp"
 
-Game::Game()
-{
-    ;
-}
+#include "enemy.hpp"
 
-Game::~Game()
-{
-    window.create(VideoMode(WIDTH, HEIGHT), "RAIDEN");
-}
+Game::Game() { ; }
 
-void Game::tick()
-{
-    ;
-}
+Game::~Game() { window.create(VideoMode(WIDTH, HEIGHT), "RAIDEN"); }
 
-void Game::run()
-{
-    Texture t_vesta, t_plane;
-    t_vesta.loadFromFile("../Resources/images/Vesta.jpg");
-    t_plane.loadFromFile("../Resources/images/plane.png");
-//    t_vesta.loadFromFile("Raiden/Resources/images/Vesta.jpg");
-//    t_plane.loadFromFile("Raiden/Resources/images/plane.png");
-    RenderWindow window(VideoMode(WIDTH, HEIGHT), "RAIDEN");
-    
-    Sprite vesta(t_vesta);
-    Sprite plane(t_plane);
-    
-    Vector2u planeSize;
-    plane.setScale(0.4, 0.4);
-    vesta.setPosition(0, 0);
-    plane.setPosition(WIDTH/2, HEIGHT * 9 / 10);
-    
-    Clock clock;
-    float timer = 0, delay = 0.1;
-    double dt;
-    float speed = 500;
-    
-    while(window.isOpen())
-    {
-        float dt = clock.restart().asSeconds();
-        clock.restart();
-        timer += dt;
-        
-        Event e;
-        while(window.pollEvent(e))
-        {
-            if(e.type == Event::Closed)
-                window.close();
-            if (e.type == Event::KeyPressed && e.key.code == Keyboard::Escape)
-                window.close();
-        }
-        
-        
-        if(Keyboard::isKeyPressed(Keyboard::W))
-        {
-            plane.move(0.f, -speed * dt);
-            if (plane.getPosition().y <= 0) //Left
-                plane.setPosition(plane.getPosition().x, 0.f);
-        }
-        if(Keyboard::isKeyPressed(Keyboard::S))
-        {
-            plane.move(0.f, speed * dt);
-            if (plane.getPosition().y >= window.getSize().y - plane.getGlobalBounds().height) //Bottom
-                plane.setPosition(plane.getPosition().x, window.getSize().y - plane.getGlobalBounds().height);
-        }
-        if(Keyboard::isKeyPressed(Keyboard::A))
-        {
-            plane.move(-speed * dt * 1.2, 0.f);
-            if (plane.getPosition().x <= 0) //Left
-                plane.setPosition(0.f, plane.getPosition().y);
-        }
-        if(Keyboard::isKeyPressed(Keyboard::D))
-        {
-            plane.move(speed * dt * 1.2, 0.f);
-            if (plane.getPosition().x >= window.getSize().x - plane.getGlobalBounds().width) //Right
-                plane.setPosition(window.getSize().x - plane.getGlobalBounds().width, plane.getPosition().y);
-        }
-        
-        if(timer > delay)
-        {
-            timer = 0;
-            tick();
-        }
-        
-        window.clear();
-        
-        
-        window.draw(vesta);
-        window.draw(plane);
-        window.display();
-        
+void Game::tick() { ; }
+
+void Game::run() {
+  Texture t_vesta, t_plane;
+  // t_vesta.loadFromFile("../Resources/images/Vesta.jpg");
+  //  t_plane.loadFromFile("../Resources/images/plane.png");
+  t_vesta.loadFromFile("Raiden/Resources/images/Vesta.jpg");
+  t_plane.loadFromFile("Raiden/Resources/images/plane.png");
+  RenderWindow window(VideoMode(WIDTH, HEIGHT), "RAIDEN");
+
+  Sprite vesta(t_vesta);
+  Sprite plane(t_plane);
+
+  Vector2u planeSize;
+  plane.setScale(0.4, 0.4);
+  vesta.setPosition(0, 0);
+  plane.setPosition(WIDTH / 2, HEIGHT * 9 / 10);
+
+  Clock clock;
+  float timer = 0, delay = 0.1;
+  double dt;
+  float speed = 500;
+
+  while (window.isOpen()) {
+    float dt = clock.restart().asSeconds();
+    clock.restart();
+    timer += dt;
+
+    Event e;
+    while (window.pollEvent(e)) {
+      if (e.type == Event::Closed) window.close();
+      if (e.type == Event::KeyPressed && e.key.code == Keyboard::Escape)
+        window.close();
     }
+
+    Enemy dog(400, 400, 1, window);
+
+    if (Keyboard::isKeyPressed(Keyboard::W)) {
+      plane.move(0.f, -speed * dt);
+      if (plane.getPosition().y <= 0)  // Left
+        plane.setPosition(plane.getPosition().x, 0.f);
+    }
+    if (Keyboard::isKeyPressed(Keyboard::S)) {
+      plane.move(0.f, speed * dt);
+      if (plane.getPosition().y >=
+          window.getSize().y - plane.getGlobalBounds().height)  // Bottom
+        plane.setPosition(plane.getPosition().x,
+                          window.getSize().y - plane.getGlobalBounds().height);
+    }
+    if (Keyboard::isKeyPressed(Keyboard::A)) {
+      plane.move(-speed * dt * 1.2, 0.f);
+      if (plane.getPosition().x <= 0)  // Left
+        plane.setPosition(0.f, plane.getPosition().y);
+    }
+    if (Keyboard::isKeyPressed(Keyboard::D)) {
+      plane.move(speed * dt * 1.2, 0.f);
+      if (plane.getPosition().x >=
+          window.getSize().x - plane.getGlobalBounds().width)  // Right
+        plane.setPosition(window.getSize().x - plane.getGlobalBounds().width,
+                          plane.getPosition().y);
+    }
+
+    if (timer > delay) {
+      timer = 0;
+      tick();
+    }
+
+    window.clear();
+
+    window.draw(vesta);
+    window.draw(plane);
+    window.display();
+  }
 }
