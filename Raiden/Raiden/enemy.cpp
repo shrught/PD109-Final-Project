a5606598c92031828e479ced44316fb5f3aed376
audreyhsiao <51323445+audreyhsiao@users.noreply.github.com>
2021-01-10 22:55:20 +0800

    enemy with life

--- Raiden/Raiden/enemy.cpp
+++ Raiden/Raiden/enemy.cpp
@@ -1,20 +1,22 @@
 #include "enemy.hpp"
+
 #include <iostream>
 using namespace std;
+using namespace sf;
 
 void Enemy::draw(sf::RenderWindow& window) {
   pdogs.setTexture(graphic);
   window.draw(pdogs);
 }
 
-Enemy::Enemy(float x_, float y_, RenderWindow& window,double size) {
+Enemy::Enemy(float x_, float y_, RenderWindow& window, double size) {
   if (!graphic.loadFromFile("Raiden/Resources/images/pdogs.png")) {
     graphic.loadFromFile("../Resources/images/pdogs.png");
   }
   // graphic.loadFromFile("../Resources/images/pdogs.png");
   pdogs.setTexture(graphic);
   pdogs.setPosition(Vector2f(x_, y_));
-  pdogs.setScale(size/10, size/10);
+  pdogs.setScale(size / 10, size / 10);
   life = 10;
   this->size = size;
   // window.draw(pdogs);
@@ -25,17 +27,16 @@ void Enemy::fly(int speed) {
   pdogs.move(0, speed);
 }
 
-void Enemy::checkCollision(FBullet bullet)
-{
-  if (bullet.getBottom() >= pdogs.getPosition().y &&
-      bullet.getLeft() >= pdogs.getPosition().x + (size/10) &&
-      bullet.getRight() <= pdogs.getPosition().x + (size/10))
+void Enemy::checkCollision(FBullet bullet) {
+  if (bullet.getTop() <=
+          pdogs.getPosition().y + pdogs.getGlobalBounds().height &&
+      bullet.getRight() >= pdogs.getPosition().x &&
+      bullet.getLeft() <= pdogs.getPosition().x) {
     life -= 1;
-    // life -= 1;
-    // plane.setPosition(Vector2f(4234432, 4234423));
-  
+    bullet.setPos(Vector2f(104800, 104800));
+    if (life <= 0) {
+      pdogs.setPosition(Vector2f(104800, 104800));
+    }
+    cout << "life " << life << endl;
+  }
 }
-
-
-
-  
--- Raiden/Raiden/enemy.hpp
+++ Raiden/Raiden/enemy.hpp
@@ -14,6 +14,7 @@ class Enemy {
   void fly(int speed);
   int life;
   void checkCollision(FBullet bullet);
+  
 
  private:
   float x_;
